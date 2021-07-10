39bf532344e7554339b735150380795123ffd6cd
hyeminpark9105 <hyeminpark9105@gmail.com>
2021-07-10 14:33:21 +0900

    ⚙️ :: swagger 세팅 수정

--- modules/swagger.js
+++ modules/swagger.js
@@ -5,11 +5,15 @@ const options = {
     swaggerDefinition: {
         openapi: '3.0.0', 
         info: {
-            title: 'Test API',
+            title: 'NewsShell API',
             version: '1.0.0',
-            description: 'Test API with express',
+            description: 'NewsShell API with express',
         },
-        host: 'localhost:3000',
+        //host: 'localhost:3000',
+        host: "127.0.0.1:" + (process.env.PORT || 3000),
+        schemes: [
+            "https"
+        ],
         basePath: '/'
     },
     apis: ['./routes/*.js', './swagger/*']
